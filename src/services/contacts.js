9ffacd5d9e5feb8a4c2672b034174dc917a5c6fb
yuliia levytska <lupin773311asd@gmail.com>
2025-05-29 22:24:49 +0100

    fvd

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -8,3 +8,10 @@ export const getContactById = async (id) => {
   return await Contact.findById(id);
 };
 
+// Новая функция для создания контакта
+export const addContact = async (contactData) => {
+  const contact = new Contact(contactData);
+  return await contact.save();
+};
+
+
