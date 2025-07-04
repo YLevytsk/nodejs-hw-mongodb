2d29928287ea066996f80042f1aaa13354d46410
yuliia levytska <lupin773311asd@gmail.com>
2025-07-04 09:21:38 +0100

    sef

--- src/validation/contactsSchemas.js
+++ src/validation/contactsSchemas.js
@@ -1,10 +1,20 @@
 import Joi from "joi";
 
+
 const name = Joi.string().min(3).max(20).required();
 const email = Joi.string().email().min(3).max(20).required();
 const phoneNumber = Joi.string().min(3).max(20).required();
-const isFavourite = Joi.boolean().optional();
-const contactType = Joi.string().valid("personal", "work", "other").optional();
+
+
+const isFavourite = Joi.boolean()
+  .truthy("true")
+  .falsy("false")
+  .default(false);
+
+const contactType = Joi.string()
+  .valid("personal", "work", "other")
+  .required(); 
+
 
 export const addContactSchema = Joi.object({
   name,
@@ -14,12 +24,14 @@ export const addContactSchema = Joi.object({
   contactType,
 });
 
+
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
   email: Joi.string().email().min(3).max(20),
   phoneNumber: Joi.string().min(3).max(20),
-  isFavourite: Joi.boolean(),
+  isFavourite: Joi.boolean().truthy("true").falsy("false"),
   contactType: Joi.string().valid("personal", "work", "other"),
 }).min(1);
+
 
 
