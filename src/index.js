9e0936d8b478b3f47869f4e1161bf0651239cc07
yuliia levytska <lupin773311asd@gmail.com>
2025-07-12 13:35:17 +0100

    уку

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 
-// Делаем dotenv первым делом!
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
-// >>> Диагностика: показываем содержимое .env и переменные из process.env
-import fs from 'fs';
-console.log('Содержимое .env:\n', fs.readFileSync(path.resolve(__dirname, '../.env'), 'utf-8'));
-console.log('process.env.JWT_ACCESS_SECRET:', process.env.JWT_ACCESS_SECRET);
-console.log('process.env.JWT_REFRESH_SECRET:', process.env.JWT_REFRESH_SECRET);
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 import { initMongoConnection } from './db/initMongoConnection.js';
 import app from './server.js';
@@ -24,7 +19,7 @@ process.on('uncaughtException', () => {
 });
 
 process.on('unhandledRejection', () => {
-  // nothing
+  
 });
 
 const bootstrap = async () => {
