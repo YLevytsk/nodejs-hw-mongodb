752c11d982cd9c8067b0221f230c24685b49ac19
yuliia levytska <lupin773311asd@gmail.com>
2025-07-12 13:36:26 +0100

    еер

--- src/server.js
+++ src/server.js
@@ -7,14 +7,14 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Больше НЕТ импортов dotenv здесь! Только в index.js!
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Глобальные обработчики ошибок
+
 process.on('uncaughtException', (err) => {
   console.error('Uncaught Exception:', err);
 });
@@ -22,23 +22,23 @@ process.on('unhandledRejection', (err) => {
   console.error('Unhandled Rejection:', err);
 });
 
-// Читаем swagger.json
+
 const swaggerJsonPath = path.join(__dirname, '../docs/swagger.json');
 const swaggerDocument = JSON.parse(fs.readFileSync(swaggerJsonPath, 'utf-8'));
 
-// Раздаем docs как статичную папку (опционально, чтобы получить доступ к swagger.json по URL)
+
 app.use('/docs', express.static(path.join(__dirname, '../docs')));
 
-// Подключаем Swagger UI
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(pinoHttp());
 
-// Роуты
+
 import contactsRouter from './routers/contactsRouter.js';
 import authRouter from './routers/auth.js';
 import authenticate from './middlewares/authenticate.js';
@@ -50,7 +50,7 @@ app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the contacts API' });
 });
 
-// Обработчики ошибок
+
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 
